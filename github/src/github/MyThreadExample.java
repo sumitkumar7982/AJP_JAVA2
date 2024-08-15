ccec93bec1bf5cbfd5690e795cd561a4c28d7a09
sumitkumar7982 <sumitkumar1682001@gmail.com>
2024-08-15 16:01:39 +0530

    fifth commit

--- github/src/github/MyThreadExample.java
+++ github/src/github/MyThreadExample.java
@@ -1,26 +1,26 @@
-//package github;
+package github;
 
 
-//class MyThread extends Thread {
-//@Override
-//public void run() {
-//// Code to be executed in the thread
-//System.out.println("Thread is running.");
-//}
-//}
-//
-//public class MyThreadExample {
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		// Step 2: Instantiate and start the thread
-//		MyThread myThread = new MyThread();
-//		myThread.start();
-//		
-//		// The main thread continues running concurrently with the new thread
-//		System.out.println("Main thread is running.");
-//
-//
-//	}
-//
-//}
+class MyThread extends Thread {
+@Override
+public void run() {
+// Code to be executed in the thread
+System.out.println("Thread is running.");
+}
+}
+
+public class MyThreadExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Step 2: Instantiate and start the thread
+		MyThread myThread = new MyThread();
+		myThread.start();
+		
+		// The main thread continues running concurrently with the new thread
+		System.out.println("Main thread is running.");
+
+
+	}
+
+}
--- github/src/github/ThreadStateExample.java
+++ github/src/github/ThreadStateExample.java
@@ -1,47 +1,47 @@
-//package github;
-//
-//class MyThread1 extends Thread {
-//	@Override
-//	public void run() {
-//		System.out.println("Thread is running.");
-//		synchronized (ThreadStateExample.class) {
-//			try {
-//				Thread.sleep(2000);
-//				ThreadStateExample.class.wait();
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		System.out.println("Thread is awake.");
-//	}
-//}
-//
-//public class ThreadStateExample {
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		Thread thread = new MyThread1();
-//		System.out.println("Thread State: " + thread.getState()); // NEW
-//		thread.start();
-//		System.out.println("Thread State: " + thread.getState()); // RUNNABLE
-//		try {
-//			Thread.sleep(1000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		System.out.println("Thread State: " + thread.getState()); // TIMED_WAITING
-//		synchronized (ThreadStateExample.class) {
-//			ThreadStateExample.class.notify();
-//		}
-//		try {
-//			Thread.sleep(1000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		System.out.println("Thread State: " + thread.getState()); // TERMINATED
-//
-//	}
-//
-//}
+package github;
+
+class MyThread1 extends Thread {
+	@Override
+	public void run() {
+		System.out.println("Thread is running.");
+		synchronized (ThreadStateExample.class) {
+			try {
+				Thread.sleep(2000);
+				ThreadStateExample.class.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Thread is awake.");
+	}
+}
+
+public class ThreadStateExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread thread = new MyThread1();
+		System.out.println("Thread State: " + thread.getState()); // NEW
+		thread.start();
+		System.out.println("Thread State: " + thread.getState()); // RUNNABLE
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Thread State: " + thread.getState()); // TIMED_WAITING
+		synchronized (ThreadStateExample.class) {
+			ThreadStateExample.class.notify();
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Thread State: " + thread.getState()); // TERMINATED
+
+	}
+
+}
